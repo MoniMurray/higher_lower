293f99828fc44f35a43a5b824988bacd505213f4
Monica Murray <Monica.rickerby@gmail.com>
2022-10-15 13:17:34 +0000

    Add Start Game button and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,69 @@
 
 
-let i = "Hello world";
-console.log(i);
+// Event listener for DOM load and Start Game button click
+
+
+function randomNumbers () {
+
+}
+
+function startGame () {
+
+}
+
+
+
 
 // Trying Canvas
-var canvas = document.getElementById("myCanvas");
-console.log("canvas");
-var c = canvas.getContext("2d");
+// var canvas = document.getElementById("myCanvas");
+// console.log("canvas");
+// var c = canvas.getContext("2d");
 
 
-function randomNum () {
-    let Num1 = Math.floor(Math.random() * 13);
-}
+// function randomNum () {
+//     let Num1 = Math.floor(Math.random() * 13);
+// }
 
-function flipCoin () {
-    // declare variable flip
-    // assign Heads=?, Tails=randomNum
-    let coinFlip = document.getElementById("flipCoin");
-    coinFlip.addEventListener() = 'click';
-    var flip = Math.floor(Math.random()*13);
-    if (flip === "?") {
-    drawHeads(200, 200, "red");
-} else {
-    drawTails(200, 200, flip);
-}
+// function flipCoin () {
+//     // declare variable flip
+//     // assign Heads=?, Tails=flip
+//     let coinFlip = document.getElementById("flipCoin");
+//     coinFlip.addEventListener() = 'click';
+//     var flip = Math.floor(Math.random()*13);
+//     if (flip === "?") {
+//     drawHeads(200, 200, "red");
+// } else {
+//     drawTails(200, 200, flip);
+// }
 
 
-function drawHeads (xloc, yloc, Red) {
-    c.beginPath();
-    c.fillstyle = Red;
-    c.strokeStyle = "Black" ;
-    c.linewidth = 5;
-    c.arc(xloc, yloc, 100, 0, 2 * Math.PI);
-    c.fill();
-    c.stroke();
-    c.closePath();
-    // draw "?" on Head side
-    c.fillstyle = "Black"
-    c.font = "72px Arial";
-    c.fillText = ("?", xloc, yloc);
-}
+// function drawHeads (xloc, yloc, Red) {
+//     c.beginPath();
+//     c.fillstyle = Red;
+//     c.strokeStyle = "Black" ;
+//     c.linewidth = 5;
+//     c.arc(xloc, yloc, 100, 0, 2 * Math.PI);
+//     c.fill();
+//     c.stroke();
+//     c.closePath();
+//     // draw "?" on Head side
+//     c.fillstyle = "Black"
+//     c.font = "72px Arial";
+//     c.fillText = ("?", xloc, yloc);
+// }
 
-function drawTails (xloc, yloc, flip) {
-    c.beginPath();
-    c.fillstyle = "red";
-    c.strokeStyle = "Black" ;
-    c.linewidth = 5;
-    c.arc(xloc, yloc, 100, 0, 2 * Math.PI);
-    c.fill();
-    c.stroke();
-    c.closePath();
-    // insert flip() on Head side
-    c.fillstyle = "Black"
-    c.font = "72px Arial";
-    c.fillText = (flip, xloc, yloc);
-}
-}
+// function drawTails (xloc, yloc, flip) {
+//     c.beginPath();
+//     c.fillstyle = "red";
+//     c.strokeStyle = "Black" ;
+//     c.linewidth = 5;
+//     c.arc(xloc, yloc, 100, 0, 2 * Math.PI);
+//     c.fill();
+//     c.stroke();
+//     c.closePath();
+//     // insert flip() on Head side
+//     c.fillstyle = "Black"
+//     c.font = "72px Arial";
+//     c.fillText = (flip, xloc, yloc);
+// }
+// }
